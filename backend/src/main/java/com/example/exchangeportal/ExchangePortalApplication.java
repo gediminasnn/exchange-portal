44445960754b9da6a3019ea442108a5be2088ef0
Gediminas <nenius.gedas@gmail.com>
2024-05-12 19:02:33 +0300

    Project init

---
+++ backend/src/main/java/com/example/exchangeportal/ExchangePortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.exchangeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangePortalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/exchangeportal/ExchangePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchangeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
